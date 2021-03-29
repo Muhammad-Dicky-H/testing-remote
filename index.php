8bddd33cc8acbfb4a7bef9b722b1c078463c7da6
Muhammad Dicky Hadiyansyah <kiky8894@gmail.com>
2021-03-29 12:49:53 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>remote</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quibusdam voluptatum vero laborum odio. Dolores veniam, dolore officiis exercitationem inventore quidem architecto omnis ex ad eius cupiditate, et, ut odit.</p>
 </body>
 </html>
